7f60e3020ddf0546a98e27b9a393a0db953872c8
Suryakant <skant758@gmail.com>
2024-04-17 14:24:00 +0530

    made some changes in login

--- app/api/listings+api.ts
+++ app/api/listings+api.ts
@@ -3,18 +3,18 @@ import { ExpoRequest, ExpoResponse } from 'expo-router/server';
 const API_KEY = process.env.CRYPTO_API_KEY;
 
 export async function GET(request: ExpoRequest) {
-  // const limit = request.expoUrl.searchParams.get('limit') || 5;
+  const limit = request.expoUrl.searchParams.get('limit') || 5;
 
-  // const response = await fetch(
-  //   `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=${limit}&convert=EUR`,
-  //   {
-  //     headers: {
-  //       'X-CMC_PRO_API_KEY': API_KEY!,
-  //     },
-  //   }
-  // );
+  const response = await fetch(
+    `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=${limit}&convert=EUR`,
+    {
+      headers: {
+        'X-CMC_PRO_API_KEY': API_KEY!,
+      },
+    }
+  );
 
-  // const res = await response.json();
+  const res = await response.json();
   // return ExpoResponse.json(res.data);
   return ExpoResponse.json(data);
 }
